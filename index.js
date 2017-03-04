61ab4fdd6fd9076d761582a7c5de05d60d855eaf
Benjamin Salinas <ben@luminairecoffee.com>
2017-03-04 15:16:09 -0600

    Make the map css rules a bit prettier

--- index.js
+++ index.js
@@ -3,30 +3,29 @@ var execSync = require('child_process').execSync;
 var bbox = require('geojson-bbox');
 var shapefile = require("shapefile");
 
-var tileMillProjectName = "road-trip";
-var pathToMapStyleSheet = "/Users/ben/Documents/MapBox/project/"+tileMillProjectName+"/highlighted_distrcit.mss";
+var tileMillProjectName = "CongressionalDistricts";
+var pathToMapStyleSheet = "/Users/ben/Documents/MapBox/project/"+tileMillProjectName+"/highlighted_district.mss";
 var pathToTileMill = "/Applications/TileMill.app/Contents/Resources";
 //Should be absolute
-var pathToExportedImages = "~/images/tx_house";
+var pathToExportedImages = "~/images/tx_house2";
 
-var template = '#districts {\n\
-line-color:#000;\n\
-  line-width:0.25;\n\
-  [NAMELSAD= "State House District {{DISTRICT_NUMBER}}"]{\n\
-    polygon-fill:#f00;\n\
-    polygon-opacity:0.5;\n\
-    line-color:#000;\n\
-    line-width:1.0;\n\
-    text-name:"[NAMELSAD]";\n\
-    text-face-name:@serif;\n\
-    text-transform:uppercase;\n\
-    text-character-spacing:1;\n\
-    text-line-spacing:4;\n\
-    text-size:20;\n\
-    text-wrap-width:120;\n\
-    text-allow-overlap:false;\n\
-    text-halo-radius:0;\n\
-  }\n\
+var template = '#districts { \n\
+line-color:rgba(100,200,100,0.8);\n\
+  line-width:0.25; \n\
+  [NAMELSAD= "State House District {{DISTRICT_NUMBER}}"]{ \n\
+    polygon-fill:#45CB41; \n\
+    polygon-opacity:0.5; \n\
+    line-color:#000; \n\
+    line-width:1.0; \n\
+    text-name:"House{{DISTRICT_NUMBER}}"; \n\
+    text-face-name:@sans; \n\
+    text-transform:uppercase; \n\
+    text-character-spacing:1; \n\
+    text-size:20; \n\
+    text-allow-overlap:true; \n\
+    text-halo-fill:rgba(255,255,255,0.75); \n\
+    text-halo-radius:2; \n\
+  } \n\
 }';
 var commandTemplate = "./index.js export {{PROJECT_NAME}} --bbox={{LAT_1}},{{LON_1}},{{LAT_2}},{{LON_2}} --format=png {{OUTPUT_PATH}}/district_{{DISTRICT_NUMBER}}.png";
 
@@ -42,15 +41,15 @@ shapefile.read("data/tl_2016_48_sldl.shp","data/tl_2016_48_sldl.dbf").then(funct
         console.log("District "+district)
 
         //Populate our District Number in our Map Stylesheet
-        var newCSS = template.replace("{{DISTRICT_NUMBER}}", district);
+        var newCSS = template.replace("{{DISTRICT_NUMBER}}", district).replace("{{DISTRICT_NUMBER}}",(district));
         //Put that in the appropriate place.
         fs.writeFileSync(pathToMapStyleSheet, newCSS, 'utf8');
         
         //Calculate the bounding box of the feature.
         var box = bbox(features[idx]);
         //We want to center our district with some margin around it.
-        var latMargin = (box[2] - box[0])/1.0;
-        var lonMargin = (box[3] - box[1])/1.0;
+        var latMargin = (box[2] - box[0])/1.2;
+        var lonMargin = (box[3] - box[1])/1.2;
 
         var marginedBox = [box[0] - latMargin, box[1] - lonMargin, box[2] + latMargin, box[3] + lonMargin];
 
